c43c8f86c50771efb95d8794e094c21218b53652
Chandra Munda <m.c.munda@gmail.com>
2024-10-29 00:34:47 +0530

    Adding two folders GUI and CongiINI

---
+++ 02 GUI Python/AutomationSequences/SPIA/Getusername.ps1
@@ -0,0 +1,9 @@
+﻿# Get current date and time
+$currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+# Get the current username
+$username = Whoami
+
+# Display the information
+Write-Host "Current Date and Time: $currentDate"
+Write-Host "Username: $username"
